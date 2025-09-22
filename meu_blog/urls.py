d8b1fdaea9a8fff915e3b8cfbb62f108a4d76387
Samara Gonçalves <samarasilva222220@gmail.com>
2025-09-22 14:55:26 -0300

    Projeto Django calculadora - CSS funcionando

---
+++ meu_blog/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for meu_blog project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from calculadora import views as calc_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('calculadora/', calc_views.index, name='calculadora'),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('calculadora/', include('calculadora.urls'), name='calculadora'),
+]
+
+# Versão mais simples e segura
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
