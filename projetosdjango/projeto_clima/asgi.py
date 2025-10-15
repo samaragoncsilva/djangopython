462b10ac810b29ac2e3e05afe96e7efc12e7c26e
Samara Gonçalves <samarasilva222220@gmail.com>
2025-10-15 15:28:22 -0300

    Renomeia pasta do projeto para projetosdjango

--- projetosdjango/projeto_clima/asgi.py
+++ projetosdjango/projeto_clima/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_clima.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetosdjango.settings')
+
 
 application = get_asgi_application()
--- projetosdjango/projeto_clima/wsgi.py
+++ projetosdjango/projeto_clima/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_clima.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetosdjango.settings')
+
 
 application = get_wsgi_application()
